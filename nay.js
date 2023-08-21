eff227d5bbc073a47869aa043ec222103527d57e
foxy242 <114094487+foxy242@users.noreply.github.com>
2023-08-21 17:11:44 +0300

    1

---
+++ nay.js
@@ -0,0 +1,29 @@
+// звданя №1
+
+console.log(5+5+'5');
+
+// звданя №2
+
+const productName = "gosha.chikilyov@gmail.com";
+console.log(productName.includes("@"));
+
+// звданя №3
+
+const my = 'my'
+
+const name = 'name'
+
+const is = 'is'
+
+const Viktor = 'Viktor'
+
+const fullName = 'my name is Viktor'
+
+console.log (fullName)
+
+// звданя №4
+
+const userName = "fox";
+const payment = 207;
+const greeting = alert;(`Welcome ${userName}, your room number is ${payment}!`);
+console.log(greeting); // "Welcome Mango, your room number is 207!"
